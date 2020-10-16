eca0aa921dfb64630141fdce93c9b47ac5957585
StarkGang <StarkGang@users.noreply.github.com>
2020-10-16 16:55:22 +0000

    bug fixes

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -120,7 +120,7 @@ if Var.PRIVATE_GROUP_ID is not None:
 
     @bot.on(events.NewMessage(incoming=True))
     async def on_new_private_message(event):
-        if event.from_id == bot.uid:
+        if event.sender_id == bot.uid:
             return
 
         if Var.PRIVATE_GROUP_ID is None:
@@ -130,7 +130,7 @@ if Var.PRIVATE_GROUP_ID is not None:
             return
 
         message_text = event.message.message
-        sender_id = event.from_id
+        sender_id = event.sender_id
 
         message_text.lower()
         if USER_BOT_NO_WARN == message_text:
